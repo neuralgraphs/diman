f9e145f0bcb76cb0164d4794627e69c5de580da9
lungsi <neuralgraphs@gmail.com>
2020-05-08 00:56:02 +0200

    snapshot to 0.0.1-Alpha

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject diman "0.0.1-3-Alpha"
+(defproject diman "0.0.1-Alpha"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -7,4 +7,5 @@
                  ;[expresso "0.2.2"]                         ; https://github.com/clojure-numerics/expresso
                  ;[org.flatland/ordered "1.5.7"]             ; https://github.com/clj-commons/ordered
                  ]
+  ;:plugins [[lein-codox "0.10.7"]                            ; https://github.com/weavejester/codox
   )
