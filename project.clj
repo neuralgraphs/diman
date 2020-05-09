2fe2cb724cc6b62e7a5a5e2e4355f5202b63df8c
lungsi <neuralgraphs@gmail.com>
2020-05-09 19:04:25 +0200

    added codex info

--- project.clj
+++ project.clj
@@ -1,11 +1,16 @@
 (defproject diman "0.0.1-Alpha"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library for applying dimensional analysis."
+  :url "https://github.com/lungsi/diman"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 ;[expresso "0.2.2"]                         ; https://github.com/clojure-numerics/expresso
-                 ;[org.flatland/ordered "1.5.7"]             ; https://github.com/clj-commons/ordered
-                 ]
-  ;:plugins [[lein-codox "0.10.7"]                            ; https://github.com/weavejester/codox
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :plugins [[lein-codox "0.10.7"]]                          ; https://github.com/weavejester/codox
+  :codox {
+          :output-path "codox"                              ; write output to codox/
+          :source-uri                                       ; document links to source files
+                       "http://github.com/lungsi/diman/blob/{version}/{filepath}#L{line}"
+          :source-paths ["src"]                             ; location of source files
+          ;:doc-files ["ProjectPlan.pdf",                    ; only supports .md or .markdown
+          ;            "doc/tutorial1.pdf"]
+          }
   )
