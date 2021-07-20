a2ef74fe9434e654a2f6bc86a3aa6e6acf656c12
lungsi <neuralgraphs@gmail.com>
2021-07-21 00:56:28 +0200

    added default-functions

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
           ;            "doc/tutorial1.pdf"]
           }
   :repl-options {:prompt (fn [ns] (str "diman <" ns "> => "));; Custom repl prompt for diman
-                 :welcome (println "Welcome to diman\nA <dim>ensional <anal>ysis tool capable of deriving dimensionless products.")
+                 :welcome (println "Welcome to diman\nA <dim>ensional <an>alysis tool capable of deriving dimensionless products.\nTo view built-in functions do (println default-functions).")
                  :init-ns diman.default
                  ;:init (println "You are in" *ns*)          ;; Top of the repl
                  }
--- src/diman/default.clj
+++ src/diman/default.clj
@@ -9,3 +9,14 @@
      [dimensionless-product :refer [get-dimensionless-products get-pi-expression]]]
     [diman [core :refer [view-matrix]]]
     ))
+
+(def default-functions
+    "\n
+    standard_formula*, update-sformula,\n
+    formula-term, formula-eqn-side, formula-eqn-side-manifold,\n
+    dimnames, consistent?,\n
+    generate-dimmat,\n
+    get-augmented-matrix, solve, get-solved-matrix,\n
+    get-dimensionless-products, get-pi-expression,\n
+    view-matrix\n
+    Asterix (*) denotes data and not a function.")
