ba96ee72bb8ed743a6cdd2d2138809792350bf77
lungsi <neuralgraphs@gmail.com>
2021-10-15 18:09:50 +0200

    added docstrings content

--- src/diman/buckingham/dimensional_matrix.clj
+++ src/diman/buckingham/dimensional_matrix.clj
@@ -22,11 +22,15 @@
             ))
 
 (defn- convert-symbol-in-varpar-to-expression [x]
-  "x is the symbol, e.g. (:symbol (first varpars))."
+  "Given a symbol, this returns a symbol in a definition, e.g. (:symbol (first varpars)),
+  in expression form such that the symbol has the exponent 1."
   (clojure.string/join [x "^" "(1)"])
   )
 
 (defn- get-notn-expt-of-expressed-symbol [varpars x]
+  "Given the definition of variables/parameters of a system and a symbol in the definition,
+  this returns a vector of two list such that the first list contains dimensional symbols and the
+  second list contains exponents for the respective symbol."
   (let [expressed_symbol (convert-symbol-in-varpar-to-expression x)
         dimformula_symbol (formula-term varpars expressed_symbol)]
     [ (notns-in-subformula dimformula_symbol)
@@ -34,6 +38,9 @@
     ))
 
 (defn- get-notn-expt-of-varpars
+  "Given the definition of variables/parameters of a system, this returns
+  a list of maps such that each map has the keys, `:symbol`, `:notn_list`, and `:expt_list`,
+  for all the symbols in the definition."
   ([varpars] (get-notn-expt-of-varpars
                varpars (:symbol (last varpars)) []))
   ([varpars curr_symbol varpar_with_notn_expt]
@@ -51,6 +58,11 @@
      )))
 
 (defn- notn-collection
+  "Given the list of maps (each map has the keys, `:symbol`, `:notn_list`, and `:expt_list`) for all the symbols
+  (in definition of variables/parameters of the system), this returns a list of dimensional symbols corresponding to
+  all the symbols in the definition. That is, returns appended list of `:notn_list` for all the symbols in the map.
+  Note that the returned list can contain the same dimensional symbol appearing more than once.
+  "
   ([notn_expt_of_varpars] (notn-collection notn_expt_of_varpars []))
   ([notn_expt_of_varpars ans]
    (if (empty? notn_expt_of_varpars)
@@ -61,11 +73,19 @@
     ))
 
 (defn- size-dimmat [notn_collection varpars]
+  "Given the list of dimensional symbols for all the symbols in the definition (of variables/parameters of the system)
+  and the definition, this returns a vector of two elements, first corresponds to row of dimensional matrix
+  (the total number of dimensional symbols) and the second corresponds to the columns (the total number of symbols in
+  the defined variables/parameters of the system)."
   [(count (distinct notn_collection))                       ; no. of rows
    (count varpars)                                          ; no. of columns
    ])
 
 (defn- get-element
+  "Given a dimensional symbol and a single map (having keys, `:symbol`, `:notn_list`, and `:expt_list`) such that if the
+  `:notn_list` of the map contains the dimensional symbol, this function returns the corresponding exponent
+  extracted from `:expt_list` of the map other wise it returns 0 as its exponent.
+  "
   ([row_notn a_varpar_with_notn_expt]
    (get-element row_notn a_varpar_with_notn_expt
                 [(:notn_list a_varpar_with_notn_expt)
@@ -84,6 +104,10 @@
     ))
 
 (defn- get-column-vector
+  "Given a vector of dimensional symbols and a single map (having keys, `:symbol`, `:notn_list`, and `:expt_list`), this
+  function returns a vector of exponents for the corresponding dimensional symbols with respect to the symbol
+  (in the map) in the defined variables/parameters of the system.
+  "
   ([all_row_notns a_varpar_with_notn_expt]
    (get-column-vector all_row_notns a_varpar_with_notn_expt []))
   ([all_row_notns a_varpar_with_notn_expt ans]
@@ -97,6 +121,10 @@
     ))
 
 (defn- get-row-vector
+  "Given a vector of dimensional symbols and a list of maps (each map having keys, `:symbol`, `:notn_list`, and `:expt_list`),
+  this function returns a vector of exponents for the corresponding dimensional symbols with respect to all the symbols
+  (in each map of the list) in the defined variables/parameters of the system.
+  "
   ([row_notn varpars_with_notn_expt]
    (get-row-vector row_notn varpars_with_notn_expt []))
   ([row_notn varpars_with_notn_expt ans]
@@ -110,6 +138,10 @@
     ))
 
 (defn- fillup-dimmat
+  "Given a list of maps (each map having keys, `:symbol`, `:notn_list`, and `:expt_list`) and a zero matrix whose size
+  corresponds to the dimensional matrix, this function returns the matrix such that the entries of the zero matrix are
+  replace by exponents of dimensional symbols for respective symbol in the defined variables/parameters of the system.
+  "
   ([all_row_notns varpars_with_notn_expt dimmat]
    (fillup-dimmat all_row_notns varpars_with_notn_expt dimmat
                   (- (count dimmat) 1) (count (first dimmat))))
@@ -124,6 +156,11 @@
     ))
 
 (defn generate-dimmat [varpars]
+  "Given the definition of variables/parameters of a  system, this function returns a matrix such that a row corresponds
+  to a dimensional symbol and a column corresponds to a symbol in the defined variables/parameters. Thus, the number of
+  rows is equal to the number of dimensional symbols appearing in all the defined symbols and the number of columns is
+  equal to the number of defined symbols.
+  "
   (let [varpars_with_notn_expt (get-notn-expt-of-varpars varpars)
         notn_collection (notn-collection varpars_with_notn_expt)
         [m n] (size-dimmat notn_collection varpars)
