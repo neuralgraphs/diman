28da38f53bc8c51df780cf0e4673eb3f30f929e2
lungsi <neuralgraphs@gmail.com>
2021-10-15 19:08:14 +0200

    added docstrings content

--- src/diman/buckingham/dimensionless_product.clj
+++ src/diman/buckingham/dimensionless_product.clj
@@ -1,11 +1,39 @@
 (ns diman.buckingham.dimensionless-product
+  "Contains function
+
+   - `get-dimensionless-products`
+   - `get-pi-expression`
+
+  Given
+  ```
+  (def varpars [ {:symbol \"x\", :quantity \"length\"}
+                 {:symbol \"v\", :quantity \"velocity\"}
+                 {:symbol \"t\", :quantity \"time\"}
+                 {:symbol \"a\", :quantity \"acceleration\"} ])
+  (def soln_matrix [ [1 -11 5 8]
+                     [0 9 -4 -7] ])
+  ```
+  to get the [set of dimensionless products](https://neuralgraphs.com/lectures/diman/lectp8.html#p4_thispage)
+  ```
+  => (get-dimensionless-products soln_matrix varpars)
+  [{:symbol \"pi0\", :expression \"x^(1)*v^(-11)*t^(5)*a^(8)\"} {:symbol \"pi1\", :expression \"v^(9)*t^(-4)*a^(-7)\"}]
+  ```
+  and to extract a particular dimensionless product from the set
+  ```
+  => (get-pi-expression (get-dimensionless-products soln_matrix varpars) \"pi0\")
+  \"x^(1)*v^(-11)*t^(5)*a^(8)\"
+  ```
+  "
   (:require [diman [attach :refer [tie-notnlist-exptlist]]
              [utilities :refer [remove-brackets]]
              [filter :refer [remove-zero-powers]]]
             ))
 
 (defn- get-exponents-for-pi
-  ([a_row_of_solved_matrix] (get-exponents-for-pi a_row_of_solved_matrix []))
+  "Given a row of the solution matrix, this function returns a list such that each element is the
+  string of the corresponding element in the row vector.
+  "
+  ([a_row_of_solution_matrix] (get-exponents-for-pi a_row_of_solution_matrix []))
   ([a_row ans]
    (if (empty? a_row)
      ans
@@ -13,6 +41,9 @@
      )))
 
 (defn- get-variables-for-pi
+  "Given the definition of variables/parameters of a system, this function returns a list such that
+  each element is symbol in the definition.
+  "
   ([varpars] (get-variables-for-pi varpars []))
   ([varpars ans]
    (if (empty? varpars)
@@ -21,22 +52,30 @@
      )))
 
 (defn get-dimensionless-products
-  ([solved_matrix varpars]
-   (get-dimensionless-products solved_matrix varpars (get-variables-for-pi varpars)
-                               (count solved_matrix) 0 []))
-  ([solved_matrix varpars variable_symbol_list m i ans]
+  "Given a solution matrix and its definition of variables/parameters of a system, this function returns
+  a vector such that each element is a map with keys, `:symbol` and `:expression`. The elements of this
+  vector represents the elements of the set of dimensionless products.
+  "
+  ([solution_matrix varpars]
+   (get-dimensionless-products solution_matrix varpars (get-variables-for-pi varpars)
+                               (count solution_matrix) 0 []))
+  ([solution_matrix varpars variable_symbol_list m i ans]
    (if (= i m)
      ans
      (let [pi_i (clojure.string/join "" ["pi" i])
-           expt_list (get-exponents-for-pi (get solved_matrix i))
+           expt_list (get-exponents-for-pi (get solution_matrix i))
            formula_pi_i (tie-notnlist-exptlist [variable_symbol_list expt_list])
            cleaned_formula (remove-brackets (remove-zero-powers formula_pi_i))]
-       (recur solved_matrix varpars variable_symbol_list m (+ i 1)
+       (recur solution_matrix varpars variable_symbol_list m (+ i 1)
               (conj ans {:symbol pi_i :expression cleaned_formula}))
        ))
     ))
 
 (defn get-pi-expression
+  "Given the set of dimensionless products (a vector of maps with keys, `:symbol` and `:expression`) and
+  the symbol of a dimensionless product (as `\"piX\"` where `X` is an integer from 0, 1, 2, ...), this
+  function returns the expression of the desired dimensionless product.
+  "
   ([all_pi desired_pi] (get-pi-expression all_pi desired_pi []))
   ([all_pi desired_pi ans]
    (if (or (empty? all_pi) (not (empty? ans)))
--- test/diman/buckingham/dimensionless_product_test.clj
+++ test/diman/buckingham/dimensionless_product_test.clj
@@ -4,21 +4,21 @@
              [dimensionless-product :refer [get-dimensionless-products get-pi-expression]]]
             ))
 ;; ============================================================================
-(def solved_matrix1 [[1 0 0 0 -11 5 8]
-                    [0 1 0 0 9 -4 -7]
-                    [0 0 1 0 -9 5 7]
-                    [0 0 0 1 15 -6 -12]])
-(def varpars1 [{:symbol "p", :dimension "some dim 1"}
-              {:symbol "q", :dimension "some dim 2"}
-              {:symbol "r", :dimension "some dim 3"}
-              {:symbol "s", :dimension "some dim 4"}
-              {:symbol "t", :dimension "some dim 5"}
-              {:symbol "u", :dimension "some dim 6"}
-              {:symbol "v", :dimension "some dim 7"}])
+(def soln_matrix1 [[1 0 0 0 -11 5 8]
+                   [0 1 0 0 9 -4 -7]
+                   [0 0 1 0 -9 5 7]
+                   [0 0 0 1 15 -6 -12]])
+(def dummy_varpars [{:symbol "p", :quantity "some dim 1"}
+                    {:symbol "q", :quantity "some dim 2"}
+                    {:symbol "r", :quantity "some dim 3"}
+                    {:symbol "s", :quantity "some dim 4"}
+                    {:symbol "t", :quantity "some dim 5"}
+                    {:symbol "u", :quantity "some dim 6"}
+                    {:symbol "v", :quantity "some dim 7"}])
 
 (deftest test-get-dimensionless-products1
   (testing "comment"
-    (let [all_pi (get-dimensionless-products solved_matrix1 varpars1)]
+    (let [all_pi (get-dimensionless-products soln_matrix1 dummy_varpars)]
       (is (= "p^(1)*t^(-11)*u^(5)*v^(8)" (get-pi-expression all_pi "pi0")))
       (is (= "q^(1)*t^(9)*u^(-4)*v^(-7)" (get-pi-expression all_pi "pi1")))
       (is (= "r^(1)*t^(-9)*u^(5)*v^(7)" (get-pi-expression all_pi "pi2")))
@@ -28,13 +28,13 @@
 ;; ============================================================================
 
 ;; ============================================================================
-(def solved_matrix2 [[1 0 0 0 -13/21 -92/63 1]
-                     [0 1 0 0 -6/7 -12/7 3/2]
-                     [0 0 1 0 5/21 37/63 -1/2]
-                     [0 0 0 1 -1/7 -13/21 0]])
+(def soln_matrix2 [[1 0 0 0 -13/21 -92/63 1]
+                   [0 1 0 0 -6/7 -12/7 3/2]
+                   [0 0 1 0 5/21 37/63 -1/2]
+                   [0 0 0 1 -1/7 -13/21 0]])
 (deftest test-get-dimensionless-products2
   (testing "comment"
-    (let [all_pi (get-dimensionless-products solved_matrix2 varpars1)]
+    (let [all_pi (get-dimensionless-products soln_matrix2 dummy_varpars)]
       (is (= "p^(1)*t^(-13/21)*u^(-92/63)*v^(1)" (get-pi-expression all_pi "pi0")))
       (is (= "q^(1)*t^(-6/7)*u^(-12/7)*v^(3/2)" (get-pi-expression all_pi "pi1")))
       (is (= "r^(1)*t^(5/21)*u^(37/63)*v^(-1/2)" (get-pi-expression all_pi "pi2")))
